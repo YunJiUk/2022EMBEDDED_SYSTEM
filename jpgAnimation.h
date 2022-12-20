dbdeb1e37b4ec66aef86bd66b0961e4fb5db6225
JiUk Yun <yjoo1123@gmail.com>
2022-12-20 13:16:09 +0900

    jpgAnimation.h_last

--- jpgAnimation.h
+++ jpgAnimation.h
@@ -1,7 +1,16 @@
 #ifndef _JPGANIMATION_H_
 #define _JPGANIMATION_H_
 
+#define GAME1MSG_ID 5555
 #define GAME2MSG_ID 7777
+
+
+typedef struct
+{
+long int messageType_jpg1;
+int game1Space;
+} structgame1Msg;
+
 
 typedef struct
 {
@@ -18,9 +27,11 @@ int AnimationPrint(char *jpg,int count_jpg);
 
 int AnimationPrint_2(char *jpg,int count_jpg);
 
-int AnimationPrint_delay(char *jpg, int numberOfJpg);
+int AnimationPrint_bath(char *jpg, int numberOfJpg);
+
+int AnimationPrint_walk();
 
 int AnimationExit(void);
 
-	
+   
 #endif
