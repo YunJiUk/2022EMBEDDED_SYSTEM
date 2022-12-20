e8f2fb73efb3de1f218e859036a4904caea91d05
JiUk Yun <yjoo1123@gmail.com>
2022-12-20 14:47:04 +0900

    jpgANimation

--- jpgAnimation.c
+++ jpgAnimation.c
@@ -10,9 +10,9 @@
 #include "gyro.h"
 #include "buzzer.h"
 
-pthread_t thread_buzzer;
-int C=1, D1=3, D2 =4, E=5, F1=6, F2 = 7, G=8, A=10, B1= 11, B2=12;
-void *buzzerFunc()
+pthread_t thread_buzzer; //buzzer thread 선언
+int C=1, D1=3, D2 =4, E=5, F1=6, F2 = 7, G=8, A=10, B1= 11, B2=12;//도레미파솔라시도 숫자화
+void *buzzerFunc() // 산책 게임중에 나오는 배경음악 쓰레드
 {   
    while(1){
     buzzerInit();
@@ -59,7 +59,8 @@ void *buzzerFunc()
     buzzerStopSong();
 }
 
-void gamewin(){
+void gamewin() //게임 승리시 나오는 브금
+{
     buzzerInit();
     buz2(A);
     usleep(350000);
@@ -88,7 +89,8 @@ void gamewin(){
     buzzerStopSong();    
 }
 
-void gamelose(){
+void gamelose()//게임 패배시 나오는 브금
+{
     buzzerInit();
     buz2(E);
     usleep(160000);
@@ -111,24 +113,27 @@ void gamelose(){
     buzzerStopSong(); 
 }
 
-pthread_t thread_gyro;
+pthread_t thread_gyro; // gyro thread 선언
 
-int space =2;
+int space =2; // 강아지의 위치를 나타내줄 변수
 
-void *gyroFunc(){
+void *gyroFunc() //기울기 쓰레드-센서에 감지값 따라 산책게임에서 강아지의 위치를 정해줌
+{
     Gyroinit();
     structgame1Msg Game1TxData;
     while(1){
     int x = Accelerometer();
     printf("%d", x);
-    if(x > 4500){
+    if(x > 4500) //4500 이상이면 위치(space)를 증가시킴
+    {
         printf("down");
         space++;
         if(space>2){
          space = 3;
       }
     }
-    else if(x < -1000){
+    else if(x < -1000) // -1000 이하면 위치(space)를 감소시킴
+    {
         printf("up");
       space--;
       if(space<2){
@@ -163,18 +168,19 @@ void *gyroFunc(){
    fb_clear();
    }
 
-int AnimationPrint_walk(){
+int AnimationPrint_walk() // 산책게임
+{
    lcdtextwrite("RunRunRunRunRun", "Tilt to Avoid!", 3);
    loseflag_01 = 0;
    int i=0;
-   pthread_create(&thread_gyro,NULL,gyroFunc,NULL);
-   pthread_create(&thread_buzzer,NULL,buzzerFunc,NULL);
+   pthread_create(&thread_gyro,NULL,gyroFunc,NULL); // 기울기 쓰레드 실행
+   pthread_create(&thread_buzzer,NULL,buzzerFunc,NULL); //배경음악 쓰레드 실행
     for (i=0; i<=19;i++)
    {
      
    char filename_01[200]={0,};
       
-   snprintf(filename_01,200,"./rundog/run_%d_%d.JPG",space, i);
+   snprintf(filename_01,200,"./rundog/run_%d_%d.JPG",space, i); // 위치, 상황에 맞는 사진 출력
       //FileRead
       int error_01=0;
       struct jpeg_decompress_struct cinfo_01;
@@ -210,21 +216,21 @@ int AnimationPrint_walk(){
       (space == 2 && i == 18) || (space == 3 && i == 8) || (space == 3 && i == 15))
       {   loseflag_01 = 1;
      printf("You lose\n");
-            break;
+            break; // 패배 상황시 종료
      }
     }
-   pthread_cancel(thread_gyro);
-   pthread_cancel(thread_buzzer);
+   pthread_cancel(thread_gyro); //기울기 쓰레드 종료
+   pthread_cancel(thread_buzzer);//배경음악 쓰레드 종료
    buzzerStopSong();
     sleep(1);
     if(loseflag_01 == 1){
       lcdtextwrite("     GAME OVER     ", "     GAME OVER     ", 3);
-        gamelose();
+        gamelose(); // 종료 음악 재생
       return 0;
     }
     else{
       lcdtextwrite("     YOU WIN     ", "     YOU WIN     ", 3);
-        gamewin();
+        gamewin(); // 승리 음악 재생
       return 1;
     }
    }
